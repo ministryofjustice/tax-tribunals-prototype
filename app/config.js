69f9b4c8ce1cb84ecd319008de43c21b719b7579
axemonkey <hi+github@clivemurray.com>
2016-07-27 16:38:50 +0100

    add copy to homepage

--- app/config.js
+++ app/config.js
@@ -5,10 +5,10 @@
 module.exports = {
 
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: "Service name goes here",
+  serviceName: "Tax Tribunals Prototype",
 
   // Default port that prototype runs on
-  port: '3000',
+  port: '3001',
 
   // Enable or disable password protection on production
   useAuth: 'true',
@@ -20,4 +20,4 @@ module.exports = {
   cookieText: 'GOV.UK uses cookies to make the site simpler. <a href="#" title="Find out more about cookies">Find out more about cookies</a>'
 
 };
-	
+
