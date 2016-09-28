8943b14337690402d5c21eb8d8f82a5dc9ad496c
axemonkey <hi+github@clivemurray.com>
2016-09-28 16:00:11 +0100

    improve the check answers page

--- app/assets/javascripts/modules/data-capture-summary.js
+++ app/assets/javascripts/modules/data-capture-summary.js
@@ -5,8 +5,23 @@ moj.Modules.dataCaptureSummary = {
 
   dependencies: [
     {
-      elementId: 'representative_details',
+      elementId: 'representative_details_row',
       dataKey: 'appellant_has_representation',
+      dataValue: 'yes'
+    },
+    {
+      elementId: 'closure_notice_row',
+      dataKey: 'dispute_type',
+      dataValue: 'closure'
+    },
+    {
+      elementId: 'application_is_late_row',
+      dataKey: 'hmrc_view',
+      dataValue: 'yes'
+    },
+    {
+      elementId: 'reasons_for_late_application_row',
+      dataKey: 'application_is_late',
       dataValue: 'yes'
     }
   ],
@@ -14,6 +29,7 @@ moj.Modules.dataCaptureSummary = {
   init: function() {
     var self = this;
 
+    self.showFeeDeterminationAnswers();
     self.showDependentItems();
   },
 
@@ -26,6 +42,22 @@ moj.Modules.dataCaptureSummary = {
       if(moj.Modules.dataStore.getItem(dep.dataKey) === dep.dataValue) {
         $('#' + dep.elementId).removeClass(self.hideClass);
       }
+    }
+  },
+
+  showFeeDeterminationAnswers: function() {
+    var $el = $('.js_fee-determination-details').eq(0),
+        html = '',
+        answers = moj.Modules.dataStore.getItem('storedAnswers');
+
+    if(answers) {
+      for(var x = 0; x < answers.length; x++) {
+        html += '<li>';
+        html += answers[x].text;
+        html += '</li>';
+      }
+
+      $el.html(html);
     }
   }
 };
