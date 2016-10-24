b022f71eddd1c78229519115aac59cf8cbcb2e51
axemonkey <hi+github@clivemurray.com>
2016-10-24 11:36:13 +0100

    other tax routing

--- app/assets/javascripts/module_loader.js
+++ app/assets/javascripts/module_loader.js
@@ -33,7 +33,11 @@
     },
 
     ucFirst: function(string) {
-      return string.charAt(0).toUpperCase() + string.slice(1);
+      if(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+      } else {
+        return false;
+      }
     }
 
   };
